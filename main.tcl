0a98952dcf2e7b2cc83258bed5f5b45a8c140d68
Soma Kai <somakai@MBa.local>
2022-11-24 17:21:44 +0900

    practice

--- main.tcl
+++ main.tcl
@@ -4,11 +4,20 @@ pack .header -side top
 label .header.lbl -text "Task Tree" 
 pack .header.lbl 
 
+canvas .header.border -height 20
+pack .header.border 
+.header.border create line 20 10 180 10 -fill black
+
 frame .body_test
 pack .body_test -side top
 
 label .body_test.tst -text "verification of frame"
 pack .body_test.tst
+
+canvas .body_test.can
+pack .body_test.can
+
+.body_test.can create line 80 75 120 75 -fill red
 
 
 
