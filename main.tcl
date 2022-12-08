e07ee8316c80f3e0dbc55ba7ae2e9d892067bd64
Soma Kai <somakai@MBa.local>
2022-12-08 15:50:17 +0900

    solve the selected and basis of text widget

--- main.tcl
+++ main.tcl
@@ -4,8 +4,52 @@
 
 set today_task {}
 set TODAY_FILE {./DB/today.txt}
+set i 0
 
 proc init_mainWindow {} {
+	global today_task
+	global TODAY_FILE
+	global selected
+	global i
+
+	#set today_task 
+	set input_stream [open $TODAY_FILE r]
+	set today_task [split [gets $input_stream] :]
+	#delete the null
+	set index [lsearch $today_task ""]
+	set today_task [lreplace $today_task $index $index]
+	
+	frame .fr_main
+	pack .fr_main -expand 1 -fill both
+
+	#make header
+	label .fr_main.tlt -text "Today's Tasks"
+	pack .fr_main.tlt -side top
+
+	#task frame
+	frame .fr_main.task
+	pack .fr_main.task -side top -fill x
+
+	button .fr_main.task.bt_gene -text "new" -command GenerateTask
+	pack .fr_main.task.bt_gene 
+
+
+	for {set i 0} {$i < [llength $today_task]} {incr i} {
+		checkbutton .fr_main.task.ck$i -text [lindex $today_task $i] -bg gray -height 2 -variable selected($i) -command save_task
+		.fr_main.task.ck$i deselect 
+		pack .fr_main.task.ck$i -expand 1 -fill x -anchor w -pady 1
+
+		bind .fr_main.task.ck$i <Button-2> {
+			global i
+			destroy .fr_main
+			make_memo $i
+		}
+	}
+}
+
+#############
+#binding function
+proc make_memo i {
 	global today_task
 	global TODAY_FILE
 	global selected
@@ -31,15 +75,23 @@ proc init_mainWindow {} {
 	button .fr_main.task.bt_gene -text "new" -command GenerateTask
 	pack .fr_main.task.bt_gene 
 
-	button .fr_main.bt_save -text "save" -command save_task
-	pack .fr_main.bt_save -side bottom
+	for {set j 0} {$j < [llength $today_task]} {incr j} {
+		if { $j == $i } {
+			frame .fr_main.task.mini$i 
+			pack .fr_main.task.mini$i -side top -fill x
+			checkbutton .fr_main.task.mini$i.ck$i -text [lindex $today_task $i] -bg gray -height 2 -variable selected($i) -command save_task
+			.fr_main.task.mini$i.ck$i deselect 
+			pack .fr_main.task.mini$i.ck$i -expand 1 -fill x -anchor w -pady 1
+			text .fr_main.task.mini$i.editor$i
+			pack .fr_main.task.mini$i.editor$i
+		}
 
-	for {set i 0} {$i < [llength $today_task]} {incr i} {
-		checkbutton .fr_main.task.ck$i -text [lindex $today_task $i] -bg gray -height 2 -variable selected($i) 
-		pack .fr_main.task.ck$i -expand 1 -fill x -anchor w -pady 1
+		checkbutton .fr_main.task.ck$j -text [lindex $today_task $j] -bg gray -height 2 -variable selected($j) -command save_task
+		.fr_main.task.ck$j deselect 
+		pack .fr_main.task.ck$j -expand 1 -fill x -anchor w -pady 1
+
 	}
 }
-
 
 init_mainWindow
 
@@ -54,6 +106,7 @@ proc GenerateTask {} {
 	entry .gen.task_name -textvariable task_new
 	pack .gen.task_name
 
+	.gen.task_name selection clear
 	bind .gen.task_name <Return> {return_gene $task_new; set task_new ""}
 
 	button .gen.bt_return -text return -command {return_gene $task_new; set task_new ""}
